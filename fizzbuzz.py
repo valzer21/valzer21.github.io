beedfdc692b96fd4fb2947cfb2e94a69cf9d516e
Valentina Guseva <98649659+valzer21@users.noreply.github.com>
2022-05-02 11:15:04 +0300

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+
+n = 1
+
+while n < 1000:
+    
+    if n % 3 == 0 and n % 5 == 0:
+        print ('FizzBuzz')
+
+    elif n % 3 == 0:
+        print ('Fizz')
+        
+    elif n % 5 == 0:
+        print ('Buzz')
+    
+    else:
+         print (n)
+    
+    n += 1    
